6aef881fd42b173e2eec682664548eed554ce487
LeTung <letungcntt@gmail.com>
2023-06-17 11:21:44 +0700

    update regex link

--- lib/src/models/rules/insert.dart
+++ lib/src/models/rules/insert.dart
@@ -332,7 +332,7 @@ class AutoFormatMultipleLinksRule extends InsertRule {
   // https://example.net/
   // URL generator tool (https://www.randomlists.com/urls) is used.
   static const _linkPattern =
-      r'(https?:\/\/|www\.)[\w-\.]+\.[\w-\.]+(\/([\S]+)?)?';
+      r'(https?:\/\/|www\.)[\w-\.]+\.[\w-\.]+(\/*([\S]+)?)?';
   static final linkRegExp = RegExp(_linkPattern, caseSensitive: false);
 
   @override

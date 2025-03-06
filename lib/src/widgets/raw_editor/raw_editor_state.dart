785acef499a02a808b7dcb888ff5ca7baed0fb79
DuNM <dunguyen20091998@gmail.com>
2025-03-06 15:06:46 +0700

    back lai case dessktop

--- lib/src/widgets/raw_editor/raw_editor_state.dart
+++ lib/src/widgets/raw_editor/raw_editor_state.dart
@@ -1,6 +1,7 @@
 import 'dart:async' show StreamSubscription, Completer;
 import 'dart:convert' show jsonDecode;
 import 'dart:math' as math;
+import 'dart:io';
 import 'dart:ui' as ui hide TextStyle;
 import 'package:flutter/services.dart';
 import 'package:collection/collection.dart';
@@ -210,6 +211,7 @@ class QuillRawEditorState extends EditorState
 
 
     final onImagePaste = widget.configurations.onImagePaste;
+    /// start: xử lý case paste type rieng mobile
     if (onImagePaste != null && typeCanPasteCurrent != null) {
       if (clipboard != null) {
         final com = Completer<bool>();
@@ -230,6 +232,7 @@ class QuillRawEditorState extends EditorState
         if (await com.future) return;
       }
     }
+    /// end: xử lý case paste type rieng mobile
 
 
     if (clipboard != null) {
@@ -304,6 +307,30 @@ class QuillRawEditorState extends EditorState
       );
 
       return;
+    }
+    /// back lại case default(desktop)
+    if (onImagePaste != null) {
+      if (clipboard != null) {
+        final reader = await clipboard.read();
+        if (!reader.canProvide(Formats.png)) {
+          return;
+        }
+        reader.getFile(Formats.png, (value) async {
+          final image = value;
+
+          final imageUrl = await onImagePaste(await image.readAll());
+          if (imageUrl == null) {
+            return;
+          }
+
+          controller.replaceText(
+            textEditingValue.selection.end,
+            0,
+            BlockEmbed.image(imageUrl),
+            null,
+          );
+        });
+      }
     }
   }
 
@@ -1074,7 +1101,7 @@ class QuillRawEditorState extends EditorState
     _didChangeTextEditingValue(controller.ignoreFocusOnTextChange);
   }
 
-  late StreamSubscription _streamPaste;
+  StreamSubscription? _streamPaste;
   SimpleFileFormat? typeCanPasteCurrent;
   void checkCanPasteTypeSupport() {
     typeCanPasteCurrent = null;
@@ -1090,17 +1117,15 @@ class QuillRawEditorState extends EditorState
   @override
   void initState() {
     super.initState();
-    checkCanPasteTypeSupport();
-    _streamPaste = const EventChannel('workcake.pancake.vn/events').receiveBroadcastStream().listen((call) async {
-      try {
+    if (Platfrom.isIOS || Platfrom.isAndroid) {
+      checkCanPasteTypeSupport();
+      _streamPaste = const EventChannel('workcake.pancake.vn/events').receiveBroadcastStream().listen((call) async {
         try {
           final Map dataNative = jsonDecode(call);
           if (dataNative['type'] == 'changed_pasteboard') checkCanPasteTypeSupport();
-        } catch (e, t) {
-          print('________$e, $t');
-        }
-      } catch (e) { }
-    });
+        } catch (e) { }
+      });      
+    }
 
     _clipboardStatus.addListener(_onChangedClipboardStatus);
 
@@ -1247,7 +1272,7 @@ class QuillRawEditorState extends EditorState
 
   @override
   void dispose() {
-    _streamPaste.cancel();
+    _streamPaste?.cancel();
     closeConnectionIfNeeded();
     _keyboardVisibilitySubscription?.cancel();
     HardwareKeyboard.instance.removeHandler(_hardwareKeyboardEvent);
